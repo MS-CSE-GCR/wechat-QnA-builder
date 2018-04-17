500b1798b5ff1a5939fc8911f2b004bf73163180
maye-msft <34230764+maye-msft@users.noreply.github.com>
2018-04-17 22:17:48 +0800

    Update testserver.js

--- testserver.js
+++ testserver.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 
-process.env.Wechat_Token='mmm'
-process.env.Ocp_Apim_Subscription_Key='2b637ba1a4fa4e59a642f2ce0d7f7ef9'
-process.env.QnA_Maker_Key='d1ae7408-d0dd-40b6-90b8-2f7bb7e7049b'
+process.env.Wechat_Token='mmm'//replace your token
+process.env.Ocp_Apim_Subscription_Key='2b637ba1a4fa4e59a642f2ce0d7f7ef9'//replace your key
+process.env.QnA_Maker_Key='d1ae7408-d0dd-40b6-90b8-2f7bb7e7049b'//replace your key
 
 
 var router = require('./router.js');

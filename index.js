e7ee6588e6b34d7ff56a193cf6ab7173c493ac61
maye-msft <34230764+maye-msft@users.noreply.github.com>
2018-04-17 21:07:54 +0800

    apply bodyparser

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = require('./router.js');
 
 const app = express();
+var bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+app.use(bodyParser.xml({
+    limit: '1MB',   // Reject payload bigger than 1 MB
+    xmlParseOptions: {
+      normalize: true,     // Trim whitespace inside text nodes
+      normalizeTags: true, // Transform tags to lowercase
+      explicitArray: false // Only put nodes in array if >1
+    }
+}));
 
 app.use('/', router);
+
+
 
 app.listen(process.env.PORT || 1337);
